2b63be114e88f2b2e9813abd3051d5a56aeba676
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-03-01 13:03:31 +0330

    main change

--- start.js
+++ start.js
@@ -1,8 +1,153 @@
 
-const courses=[
-    {
-        id:'1',
-        name:'sam',
-        
+
+        /*var a=2;
+var b=3;
+var c="i am";
+a=a+3;
+b=b+5;
+c=c+" string";
+console.log(a,b,c);
+//var برای تعریف همه متغیر ها 
+//let برای جاهای خاص برای یه قسمت 
+//const یه عدد ثابت کلا     
+var FristName=0;
+var FristName1="محمد سام ";
+FristName=FristName1.length;
+console.log(FristName);
+var firsLetterOflast="";
+var last="sam";
+firsLetterOflast=last[2];
+console.log(firsLetterOflast);
+var Myarray=["sam1","sam","dog"]; تابع  pop برای حذف عنصر اخر 
+  Myarray.pop();
+console.log(Myarray); 
+var Array=["novamber","septamber","pleas"]    برای حذف shift تابع 
+myDelet=Array.shift();
+console.log(Array);
+const variable=true;
+console.log(typeof variable);
+let  j=0;
+while(j<9)
+{
+  console.log(j)
+  j++;
+}
+
+// => Hello world!// => Hello QuickRef.ME
+// Prints error message to stderr
+*/
+ 
+// console.log(courses[1].Name);
+// const resault=JSON.stringify(courses);
+// console.log(resault);
+
+// for(let c of courses)
+// {
+//   console.log(c);
+
+// // }
+// courses.forEach(function(c){
+//   console.log(c);
+// const newcours=courses.filter(function(two){
+//   return two.Iscomplate===false
+// }).map(function(c){
+//  return c.Name;
+// console.log(newcours);
+// })
+// // })
+// let x="10";
+// if(x===10){
+//   console.log("x=10");
+// }
+// else{
+//   console.log("sam god");
+// }
+/*
+const courses =[
+  { 
+    id:1,
+    Name:'html',
+    time:19,
+    Iscomplate:true
+
+  },
+  { 
+    id:2,
+    Name:'css',
+    time:12,
+     Iscomplate:false
+
+  }
+]
+const addnum= (num1=20,num2=30)=> num1+num2;
+addnum(10,20);
+console.log(addnum(10,20));
+courses.forEach(x=>console.log(x));
+class Person{
+  constructor(firstName,lastName,birthDay){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.birthDay=birthDay;
+  }
+  fullname(){
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const student1=new Person('sam','samani','2001')
+console.log(student1);
+
+// choice.innerHTML="<h1>sam</h1>";
+ // choice.style.background="red";
+ //برای اینکه بفهمی از چیا میتونی استفاده کنی اینو بزن تو همین console.log(e)
+*/
+
+// console.log(e.target.className);
+// e.target.className="error";
+// ul.children[1].innerHTML="gang";
+// const choice=document.querySelector("#my-form");
+// const btn=document.querySelector(".btn");
+// btn.addEventListener("mouseout", (e)=>{
+ 
+//   document.querySelector(".items").children[1].innerHTML="<h1>sam</h1>";
+// });
+const form=document.querySelector("#my-form");
+const inputusername=document.querySelector("#name");
+const msage=document.querySelector(".msg");
+const emmail=document.querySelector("#email")
+const users=document.querySelector("#users")
+form.addEventListener('submit',onsubmit)
+
+
+function onsubmit(e)
+{  
+e.preventDefault()
+  console.log( inputusername.value)
+  if( inputusername.value===''|| inputusername.value ==='')
+  {
+    msage.innerHTML="this is error"
+    msage.classList.add("error")
+
+    setTimeout(()=>{
+      msage.innerHTML='';
+      msage.classList.remove("error");
+  },3000)
+}
+else{
+  const li=document.createElement("li")
+  li.appendChild(document.createTextNode(inputusername.value+""+emmail.value));
+  users.appendChild(li);
+  inputusername.value="";
+  emmail.value="";
+
+}
+}
+
+
+
+
+
+
+
     }
 ]
