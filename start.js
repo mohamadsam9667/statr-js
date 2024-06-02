15816dcb89d1f053d68ad88329e89d83265f7cc0
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-06-02 12:38:35 +0330

    new methode for 'click user'

--- start.js
+++ start.js
@@ -2019,58 +2019,60 @@ var kala_name = prompt("نام کالا شما چیست؟");
 
 // }
 // let print=document.querySelector(".type");
-let username=document.querySelector(".username");
-let password=document.querySelector(".password");
-let validationUser=document.querySelector(".user-validation");
-let validationPassword=document.querySelector(".password-validation");
-let printer=document.querySelector(".type");
-console.log(printer)
-// console.log(validationUser,validationPassword,username,password);
-function exitUser(){
+// let username=document.querySelector(".username");
+// let password=document.querySelector(".password");
+// let validationUser=document.querySelector(".user-validation");
+// let validationPassword=document.querySelector(".password-validation");
+// let printer=document.querySelector(".type");
+// console.log(printer)
+// // console.log(validationUser,validationPassword,username,password);
+// function exitUser(){
 
-  if(username.value.length>12)
-    {
-      validationUser.style.color='red';
-      validationUser.style.display='block';
-      validationUser.innerHTML="چون اسمت بیشتر  از 12 حرفه پس کسخلی";
+//   if(username.value.length>12)
+//     {
+//       validationUser.style.color='red';
+//       validationUser.style.display='block';
+//       validationUser.innerHTML="چون اسمت بیشتر  از 12 حرفه پس کسخلی";
 
-    }
+//     }
     
-    else{
-      validationUser.style.color='green';
-      validationUser.style.display='block';
-      validationUser.innerHTML="چون اسمت کمتر از 12 حرفه پس تو بچه خوبی هستی";
-    }
-}
+//     else{
+//       validationUser.style.color='green';
+//       validationUser.style.display='block';
+//       validationUser.innerHTML="چون اسمت کمتر از 12 حرفه پس تو بچه خوبی هستی";
+//     }
+// }
 
-function exitPass(){
-  if(password.value.length>8)
-    {
-     validationPassword.style.display='block';
-     validationPassword.style.color='green';
-     validationPassword.innerHTML="چون پسورد بیشتر از 8 حرفه تو سیگمایی"; 
-    }
-    else{
-      validationPassword.style.display='block';
-      validationPassword.style.color='red';
-      validationPassword.innerHTML="چون پسورد کمتر از 8 حرفه تو کسخلی هیچی نمیشی میدونم باباتم بهت گفته";    
-    }
+// function exitPass(){
+//   if(password.value.length>8)
+//     {
+//      validationPassword.style.display='block';
+//      validationPassword.style.color='green';
+//      validationPassword.innerHTML="چون پسورد بیشتر از 8 حرفه تو سیگمایی"; 
+//     }
+//     else{
+//       validationPassword.style.display='block';
+//       validationPassword.style.color='red';
+//       validationPassword.innerHTML="چون پسورد کمتر از 8 حرفه تو کسخلی هیچی نمیشی میدونم باباتم بهت گفته";    
+//     }
 
-}
-
-
+// }
 
 
-function login(){
-  validationPassword.style.display="none";
-  validationUser.style.dispalay="none";
-  // validationUser.innerHTML="";
-  username.value="";
-  password.value=""
-  printer.style.display='block';
-  printer.style.color="yellow";
-  printer.style.width="200px"
-}
+
+
+// function login(){
+//   validationPassword.style.display="none";
+//   validationUser.style.dispalay="none";
+//   // validationUser.innerHTML="";
+//   username.value="";
+//   password.value=""
+//   printer.style.display='block';
+//   printer.style.color="yellow";
+//   printer.style.width="200px"
+// }
+
+
 
 
 
