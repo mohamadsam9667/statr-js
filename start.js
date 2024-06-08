2f62c1f10c1de642191052dcdcd7cfa8394626d5
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-06-08 22:56:50 +0330

    discription

--- start.js
+++ start.js
@@ -2299,73 +2299,97 @@ var kala_name = prompt("نام کالا شما چیست؟");
 // }
 
 
-let inputText=document.getElementById('converter');
-let Celsios=document.querySelector('.C')
-let Farenhight=document.querySelector('.F');
-let Converter=document.getElementById('converter');
-let resault =document.querySelector('.result')
+// let inputText=document.getElementById('converter');
+// let Celsios=document.querySelector('.C')
+// let Farenhight=document.querySelector('.F');
+// let Converter=document.getElementById('converter');
+// let resault =document.querySelector('.result')
 
 
 //  F = (1.8 × C) + 32
-function convert(){
-  let TempC=Number(inputText.value);
-  let TempF=Number(inputText.value);
+// function convert(){
+//   let TempC=Number(inputText.value);
+//   let TempF=Number(inputText.value);
  
  
-  if(Celsios.innerHTML=='°C'){
-     TempC=Number(inputText.value);
-     TempF=(1.8*TempC)+32;
-   resault.innerHTML=TempF;
-  }
+//   if(Celsios.innerHTML=='°C'){
+//      TempC=Number(inputText.value);
+//      TempF=(1.8*TempC)+32;
+//    resault.innerHTML=TempF;
+//   }
 
-  else{
-    TempC=(TempF-32)/1.8;
-    resault.innerHTML=TempC;
-  }
+//   else{
+//     TempC=(TempF-32)/1.8;
+//     resault.innerHTML=TempC;
+//   }
 
 
-}
+// }
   
-   function reset(){
-    inputText.value=" "
-    resault.innerHTML="";
+//    function reset(){
+//     inputText.value=" "
+//     resault.innerHTML="";
  
  
-  }
+//   }
     
-    function change(){
+//     function change(){
      
      
-      if(Celsios.innerHTML=='°C')
-      {
-        Celsios.innerHTML='°F';
-        Farenhight.innerHTML="°C";
-      }
+//       if(Celsios.innerHTML=='°C')
+//       {
+//         Celsios.innerHTML='°F';
+//         Farenhight.innerHTML="°C";
+//       }
 
-      else{
-        Celsios.innerHTML="°C";
-        Farenhight.innerHTML="°F";
-      }
+//       else{
+//         Celsios.innerHTML="°C";
+//         Farenhight.innerHTML="°F";
+//       }
 
-      if(document.title==='SalzLearn| Js | °C to °F'){
-        document.title="SalzLearn| Js | °F to °C ";
-      }
+//       if(document.title==='SalzLearn| Js | °C to °F'){
+//         document.title="SalzLearn| Js | °F to °C ";
+//       }
 
-      else{
-         document.title="SalzLearn| Js | °C to °F";
-      }
+//       else{
+//          document.title="SalzLearn| Js | °C to °F";
+//       }
 
 
-      if(Converter.placeholder==="°C"){
-        Converter.placeholder='°F';
-      }
+//       if(Converter.placeholder==="°C"){
+//         Converter.placeholder='°F';
+//       }
 
-      else{
-        Converter.placeholder='°C';
-      }
+//       else{
+//         Converter.placeholder='°C';
+//       }
   
-  };
+//   };
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let inputType=document.getElementById('converter')
+
+inputType.username="dam";
+console.log(inputType.username)
+console.log(inputType.getAttribute("username"))
+
+
+
+
+
 
 
 
