74784e4d454ead9f16e3734d8d2387bb4422decd
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-10-21 13:56:28 +0330

    try and Throw and Catch

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,70 @@
 // console.log("sam is sigma")
 
-let Range=document.getElementById('range');
-let Display=document.querySelector('.container')
-console.log(Range)
+// let Range=document.getElementById('range');
+// let Display=document.querySelector('.container')
+// console.log(Range)
 
-Range.addEventListener('change',function(){
- console.log(Range.value)
- Display.style.filter=`brightness(${Range.value}%)`
+// Range.addEventListener('change',function(){
+//  console.log(Range.value)
+//  Display.style.filter=`brightness(${Range.value}%)`
 
-})
+// })
+
+
+
+// let divElem=document.querySelector('.title')
+// console.log(divElemStyles.width)
+// let Stile=getComputedStyle(divElem)
+// console.log(Stile.backgroundColor)
+
+
+
+
+
+
+
+
+
+let btn=document.getElementById('btn')
+let title=document.getElementById('title')
+
+function handel(){
+try{
+    // x=v
+if(title.value===18){
+throw '18'
+}else{
+    throw 'not 18'
+}
+
+
+    console.log('sam')
+}catch(eeror){
+    title.innerHTML=eeror
+}
+}
+btn.addEventListener('click',handel)
+
+
+
+// console.log(btn,'\n',title)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
