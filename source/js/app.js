eb593f731c44f567d1d8433db50c3142142740c2
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-10-23 19:57:47 +0330

    start learn how to using time in js NEW DATE()

---
+++ source/js/app.js
@@ -0,0 +1,57 @@
+// let table=document.querySelector('.table')
+// let btnAdd=document.querySelector('.btn-warning')
+// let title=document.getElementById('title')
+// let author=document.getElementById('author')
+// let year=document.getElementById('year')
+
+// console.log(table)
+// console.log(btnAdd,title,author,year)
+
+
+
+
+
+// function addItem(event){
+//     event.preventDefault();
+//     trElem=document.createElement('tr')
+
+//     thElem1=document.createElement('th')
+//     thElem1.innerHTML=title.value
+//     thElem2=document.createElement('th')
+//     thElem2.innerHTML=author.value
+
+//     thElem3=document.createElement('th')
+//     thElem3.innerHTML=year.value
+
+    // trElem.appendChild(thElem1,thElem2,thElem3)
+    // // console.log(trElem)
+    // table.append(trElem)
+    // trElem.appendChild(thElem1);
+    // trElem.appendChild(thElem2);
+    // trElem.appendChild(thElem3);
+  
+    // Append the row to the table
+//     table.append(trElem);
+//     title.value = '';
+//   author.value = '';
+//   year.value = '';
+
+// }
+
+// btnAdd.addEventListener('click',addItem)
+
+
+// console.log("sam")
+
+
+let MyTime=new Date()
+
+console.log(MyTime.getHours())
+
+
+
+
+
+
+
+
