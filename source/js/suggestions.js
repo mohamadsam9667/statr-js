87dbf5016ef56a9d3c395d2ae057ed2be777412a
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-11-06 18:43:45 +0330

    create inpute search for user mini project search`

---
+++ source/js/suggestions.js
@@ -0,0 +1,79 @@
+let suggestions = [
+    "Channel",
+    "CodingLab",
+    "CodingNepal",
+    "YouTube",
+    "YouTuber",
+    "YouTube Channel",
+    "Blogger",
+    "Bollywood",
+    "Vlogger",
+    "Vechiles",
+    "Facebook",
+    "Freelancer",
+    "Facebook Page",
+    "Designer",
+    "Developer",
+    "Web Designer",
+    "Web Developer",
+    "Login Form in HTML & CSS",
+    "How to learn HTML & CSS",
+    "How to learn JavaScript",
+    "How to become Freelancer",
+    "How to become Web Designer",
+    "How to start Gaming Channel",
+    "How to start YouTube Channel",
+    "What does HTML stands for?",
+    "What does CSS stands for?",
+];
+
+    let showSuggest=document.querySelector('.autocom-box')
+    let wrapper=document.querySelector('.wrapper')
+    let searchInput=document.querySelector('.search-input')
+// console.log(showSuggest)
+    
+
+
+inp.addEventListener('keyup',function(){
+    let Text=inp.value.toLowerCase()
+    // console.log(Text)
+
+
+    let TextUser=suggestions.filter(function(word){
+        return word.toLowerCase().includes(Text);
+    })
+    console.log(TextUser)
+    showSuggest.innerHTML = '';
+    if(TextUser.length>0){
+        wrapper.classList.add('active')
+
+        TextUser.forEach(function(event){
+            let newDiv=document.createElement('li')
+            newDiv.innerHTML=event
+            // newDiv.style.color='black'
+            searchInput.classList.add('active')
+            showSuggest.append(newDiv)
+
+
+            newDiv.addEventListener('click',function(e){
+                    console.log(e.target.innerText)
+                    inp.value=e.target.innerText
+
+            })
+
+
+        })
+    }else{
+        wrapper.classList.remove('active')
+
+    }
+    
+})
+
+
+
+
+
+
+
+
