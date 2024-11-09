5cb98e364b4f8f691c98ce580f67dedd485dbba7
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-11-10 00:18:44 +0330

    start learn indexDB

--- source/js/suggestions.js
+++ source/js/suggestions.js
@@ -1,74 +1,117 @@
-let suggestions = [
-    "Channel",
-    "CodingLab",
-    "CodingNepal",
-    "YouTube",
-    "YouTuber",
-    "YouTube Channel",
-    "Blogger",
-    "Bollywood",
-    "Vlogger",
-    "Vechiles",
-    "Facebook",
-    "Freelancer",
-    "Facebook Page",
-    "Designer",
-    "Developer",
-    "Web Designer",
-    "Web Developer",
-    "Login Form in HTML & CSS",
-    "How to learn HTML & CSS",
-    "How to learn JavaScript",
-    "How to become Freelancer",
-    "How to become Web Designer",
-    "How to start Gaming Channel",
-    "How to start YouTube Channel",
-    "What does HTML stands for?",
-    "What does CSS stands for?",
-];
+// let suggestions = [
+//     "Channel",
+//     "CodingLab",
+//     "CodingNepal",
+//     "YouTube",
+//     "YouTuber",
+//     "YouTube Channel",
+//     "Blogger",
+//     "Bollywood",
+//     "Vlogger",
+//     "Vechiles",
+//     "Facebook",
+//     "Freelancer",
+//     "Facebook Page",
+//     "Designer",
+//     "Developer",
+//     "Web Designer",
+//     "Web Developer",
+//     "Login Form in HTML & CSS",
+//     "How to learn HTML & CSS",
+//     "How to learn JavaScript",
+//     "How to become Freelancer",
+//     "How to become Web Designer",
+//     "How to start Gaming Channel",
+//     "How to start YouTube Channel",
+//     "What does HTML stands for?",
+//     "What does CSS stands for?",
+// ];
 
-    let showSuggest=document.querySelector('.autocom-box')
-    let wrapper=document.querySelector('.wrapper')
-    let searchInput=document.querySelector('.search-input')
-// console.log(showSuggest)
+//     let showSuggest=document.querySelector('.autocom-box')
+//     let wrapper=document.querySelector('.wrapper')
+//     let searchInput=document.querySelector('.search-input')
+// // console.log(showSuggest)
     
 
 
-inp.addEventListener('keyup',function(){
-    let Text=inp.value.toLowerCase()
-    // console.log(Text)
+// inp.addEventListener('keyup',function(){
+//     let Text=inp.value.toLowerCase()
+//     // console.log(Text)
 
 
-    let TextUser=suggestions.filter(function(word){
-        return word.toLowerCase().includes(Text);
-    })
-    console.log(TextUser)
-    showSuggest.innerHTML = '';
-    if(TextUser.length>0){
-        wrapper.classList.add('active')
+//     let TextUser=suggestions.filter(function(word){
+//         return word.toLowerCase().includes(Text);
+//     })
+//     console.log(TextUser)
+//     showSuggest.innerHTML = '';
+//     if(TextUser.length>0){
+//         wrapper.classList.add('active')
 
-        TextUser.forEach(function(event){
-            let newDiv=document.createElement('li')
-            newDiv.innerHTML=event
-            // newDiv.style.color='black'
-            searchInput.classList.add('active')
-            showSuggest.append(newDiv)
-
-
-            newDiv.addEventListener('click',function(e){
-                    console.log(e.target.innerText)
-                    inp.value=e.target.innerText
-
-            })
+//         TextUser.forEach(function(event){
+//             let newDiv=document.createElement('li')
+//             newDiv.innerHTML=event
+//             // newDiv.style.color='black'
+//             searchInput.classList.add('active')
+//             showSuggest.append(newDiv)
 
 
-        })
-    }else{
-        wrapper.classList.remove('active')
+//             newDiv.addEventListener('click',function(e){
+//                     console.log(e.target.innerText)
+//                     inp.value=e.target.innerText
 
-    }
+//             })
+
+
+//         })
+//     }else{
+//         wrapper.classList.remove('active')
+
+//     }
     
-})
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let users=[
+//     {id:1,name:"sam",age:20},
+//     {id:2,name:"dam",age:27},
+//     {id:3,name:"bam",age:22}
+// ]
+
+// let falseValue=Boolean(0&&{})
+// let trueValue=Boolean("0"&&{})
+// console.log("Boolean(0&&{})",falseValue)
+// console.log("Boolean('0'&&{})",trueValue)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
